07e15b2d71839514e7fb6deefcf3dc5b32a12abb
Harry Singh <me@harryio.com>
2016-04-17 13:28:06 +0530

    Add functionality to capture and view images

--- app/src/main/java/com/harryio/storj/MainActivity.java
+++ app/src/main/java/com/harryio/storj/MainActivity.java
@@ -1,13 +1,167 @@
 package com.harryio.storj;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import permissions.dispatcher.NeedsPermission;
+import permissions.dispatcher.OnPermissionDenied;
+import permissions.dispatcher.OnShowRationale;
+import permissions.dispatcher.PermissionRequest;
+import permissions.dispatcher.RuntimePermissions;
+
+import static android.os.Environment.DIRECTORY_PICTURES;
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
+@RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_CAPTURE_IMAGE = 100;
+
+    private static final String IMAGE_DIRECTORY_NAME = "Storj";
+
+    @Bind(R.id.recyclerView)
+    RecyclerView recyclerView;
+
+    private File imageFolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        MainActivityPermissionsDispatcher.setUpImageFolderWithCheck(this);
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        File imageFolder = getImageFolder();
+
+        if (imageFolder != null) {
+            File[] imageFiles = imageFolder.listFiles();
+            ImageAdapter adapter = new ImageAdapter(this, imageFiles);
+            recyclerView.setAdapter(adapter);
+        }
+    }
+
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void setUpImageFolder() {
+        imageFolder = getImageFolder();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case REQUEST_CODE_CAPTURE_IMAGE:
+                    //todo upload camera image here
+
+                    if (imageFolder != null) {
+                        ImageAdapter adapter = new ImageAdapter(this, imageFolder.listFiles());
+                        recyclerView.swapAdapter(adapter, false);
+                    }
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        MainActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
+
+    private void showRationaleMessageDialog(String message,
+                                            final PermissionRequest permissionRequest) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        permissionRequest.proceed();
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        permissionRequest.cancel();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.take_picture_button:
+                captureImage();
+                break;
+        }
+    }
+
+    void captureImage() {
+        Uri fileUri = getOutputMediaFileUri();
+
+        if (fileUri != null) {
+            Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+            startActivityForResult(captureIntent, REQUEST_CODE_CAPTURE_IMAGE);
+        }
+    }
+
+    private Uri getOutputMediaFileUri() {
+        return Uri.fromFile(getOutputMediaFile());
+    }
+
+    private File getOutputMediaFile() {
+        String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+                .format(new Date());
+
+        return imageFolder == null ? null : new File(imageFolder.getPath() +
+                File.separator + "IMG_" + timestamp + ".jpg");
+    }
+
+    private File getImageFolder() {
+        File mediaStorageDir = new File(getExternalStoragePublicDirectory(DIRECTORY_PICTURES),
+                IMAGE_DIRECTORY_NAME);
+
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs())
+                return null;
+        }
+
+        return mediaStorageDir;
+    }
+
+    @OnShowRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showRationaleForWriteExternalStorage(PermissionRequest permissionRequest) {
+        showRationaleMessageDialog("You need to provide access to external storage",
+                permissionRequest);
+    }
+
+    @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    void showDeniedPermissionMessage() {
+        Toast.makeText(MainActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
     }
 }
